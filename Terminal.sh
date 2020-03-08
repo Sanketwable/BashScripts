531797c40ee23c69c5351c6fd788816145b99bf6
sanket <sanketwable123@gmail.com>
2020-03-08 18:53:59 +0530

    adding Cool Terminal files to terminal.sh

---
+++ Terminal.sh
@@ -0,0 +1,11 @@
+
+echo 'export PS1="\[\033[1;37m\]Wable\[\033[0;36m\]♕\[\033[0;35m\] hAcK💚 \[\033[1;37m\]:\[\033[0;33m\]\W\[\033[0;37m\] ₹ "' >>~/.bash_profile
+echo 'export CLICOLOR=1' >>~/.bash_profile
+echo 'export LSCOLORS=gxfxcxdxbxegedabagaced' >>~/.bash_profile
+echo 'alias ls="ls -GFh" ' >>~/.bash_profile
+
+echo "Done Thanks for using"
+
+
+# replace Wable by "YourName"
+# replace Hack by "Your hack"
